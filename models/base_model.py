0ead858226d2a5308d1298d233afaf55a7cf4cde
7gaar <m3gamind10@gmail.com>
2024-03-08 22:40:10 +0300

    pycodestyle fixed

--- models/base_model.py
+++ models/base_model.py
@@ -23,11 +23,11 @@ class BaseModel():
                             datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 else:
                     setattr(self, key, value)
-            else:
-                self.id = str(uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = datetime.now()
-                models.storage.new(self)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Returns the dictionary"""
