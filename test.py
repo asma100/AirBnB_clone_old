702f5f179548e53bc89faf41f0fe8a12d2f0164a
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-09 23:07:16 +0200

    test files-storagenoto

--- test.py
+++ test.py
@@ -1,82 +1,51 @@
 #!/usr/bin/python3
 """
-Unittest Module for FileStorage
+Unittest for base_model
 """
+
 import unittest
-from models import storage
-from models.user import User
+import models
 from models.base_model import BaseModel
-from models.engine import file_storage
-from models.engine.file_storage import FileStorage
-import os
-
-
-class TestFileStorage(unittest.TestCase):
-    ''' Unittest for FileStorage class '''
-
-    def test_Instantiation(self):
-        ''' checks instance is of class BaseModel '''
-        obj = FileStorage()
-        self.assertIsInstance(obj, FileStorage)
-
-    def test_Access(self):
-        ''' test read-write access permissions '''
-        rd = os.access('models/engine/file_storage.py', os.R_OK)
-        self.assertTrue(rd)
-        wr = os.access('models/engine/file_storage.py', os.W_OK)
-        self.assertTrue(wr)
-        ex = os.access('models/engine/file_storage.py', os.X_OK)
-        self.assertTrue(ex)
-
-    def test_new(self):
+from datetime import datetime
+class TestBaseModel(unittest.TestCase):
+    """
+    Test class for the BaseModel class
+    """
+    def setUp(self):
         """
-        Tests method: new (saves new object into dictionary)
+        Set up method that runs before each test
         """
-        m_storage = FileStorage()
-        instances_dic = m_storage.all()
-        Aman = User()
-        Aman.id = 999999
-        Aman.name = "Aman"
-        m_storage.new(Aman)
-        key = Aman.__class__.__name__ + "." + str(Aman.id)
-        self.assertIsNotNone(instances_dic[key])
-
-    def test_reload(self):
+        self.base_model = BaseModel()
+    
+    def test_two_amenities_unique_ids(self):
         """
-        Tests method: reload (reloads objects from string file)
+        Test if the two amenities have different ids
         """
-        a_storage = FileStorage()
-        try:
-            os.remove("file.json")
-        except:
-            pass
-        with open("file.json", "w") as f:
-            f.write("{}")
-        with open("file.json", "r") as r:
-            for line in r:
-                self.assertEqual(line, "{}")
-        self.assertIs(a_storage.reload(), None)
-
-    def test_funcdocs(self):
-        ''' testing functions docstring '''
-        for func in dir(FileStorage):
-            self.assertTrue(len(func.__doc__) > 0)
-
-    def test_save(self):
-        ''' tests save method'''
-        obj = FileStorage()
-        new_obj = BaseModel()
-        obj.new(new_obj)
-        dict1 = obj.all()
-        obj.save()
-        obj.reload()
-        dict2 = obj.all()
-        for key in dict1:
-            key1 = key
-        for key in dict2:
-            key2 = key
-        self.assertEqual(dict1[key1].to_dict(), dict2[key2].to_dict())
-
+        a1 = BaseModel()
+        a2 = BaseModel()
+        self.assertNotEqual(a1.id, a2.id)
+    def test_create_time_type(self):
+        """
+        Test if the created_at attribute is a datetime object
+        """
+        self.assertIsInstance(self.base_model.created_at, datetime)
+    def test_update_time_type(self):
+        """
+        Test if the updated_at attribute is a datetime object
+        """
+        self.assertIsInstance(self.base_model.updated_at, datetime)
+    def test_str_method(self):
+        """Tests method is printing """
+        b1 = BaseModel()
+        b1printed = b1.__str__()
+        self.assertEqual(b1printed,
+                         "[BaseModel] ({}) {}".format(b1.id, b1.__dict__))
+    def test_attribute(self):
+        """Tests if the instance of BaseModel have been correctly made"""
+        self.assertTrue(hasattr(self.base_model, "__init__"))
+        self.assertTrue(hasattr(self.base_model, "created_at"))
+        self.assertTrue(hasattr(self.base_model, "updated_at"))
+        self.assertTrue(hasattr(self.base_model, "id"))
 
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()
