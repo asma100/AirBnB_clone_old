9caed661a0f37e8028156de09310966908171589
7gaar <m3gamind10@gmail.com>
2024-03-09 18:21:02 +0300

    fixed file path name

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,7 +48,7 @@ class FileStorage:
             with open(self.__file_path, 'r', encoding="UTF-8") as f:
                 new_obj_dict = json.load(f)
             for key, value in new_obj_dict.items():
-                obj = self.class_dict[value['__class__']](**value)
+                obj = self.definedClasses[value['__class__']](**value)
                 self.__objects[key] = obj
         except FileNotFoundError:
             pass
