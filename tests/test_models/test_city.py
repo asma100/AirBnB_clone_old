6ac6c042e01c6ecc8210b9db070a2e67501fe5ef
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-09 21:45:22 +0200

    test files-storagenoto

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
 """test City class""""
 import unittest
 from models.city import City
@@ -6,14 +6,34 @@ class TestAmenity(unittest.TestCase):
     """
     Test class for the City class
     """
-    def test_init(self):
+    def setUp(self):
+        """Create a City instance before each test method."""
+        self.city = City()
+    def test_city_id(self):
         """
         Test if the City class is initialized properly
-
         """
-        self.city = City()
-        self.assertEqual(self.city.city_id, "")
-        
-        
-
-
+        self.city.city_id = "city_id"
+        self.assertEqual(self.city.city_id, "city_id")
+    def test_city_id_str(self):
+        """
+        Test if the City class is stringified properly
+        """
+        self.assertEqual(str, type(self.city.city_id))
+    def test_two_cities_unique_ids(self):
+        """
+        Test if the two cities have different ids
+        """
+        a1 = City()  
+        a2 = City()
+        self.assertNotEqual(a1.id, a2.id)
+    def test_create_time_type(self):
+        """
+        Test if the created_at attribute is a datetime object
+        """
+        self.assertIsInstance(self.city.created_at, datetime)
+    def test_update_time_type(self):
+        """
+        Test if the updated_at attribute is a datetime object 
+        """
+        self.assertIsInstance(self.city.updated_at, datetime) 
